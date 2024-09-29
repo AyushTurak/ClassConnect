ab5ae242432af46e56cb8353acf6c0162f6bb3e0
RiyaanVerma <riyaanverma95@gmail.com>
2024-09-29 08:56:30 +0530

    Login and Sign up UI

--- app/src/main/java/com/example/classconnect/Authentication/SignUp/SignUp.kt
+++ app/src/main/java/com/example/classconnect/Authentication/SignUp/SignUp.kt
@@ -16,6 +16,21 @@ class SignUp : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+            val roleSpinner: Spinner = findViewById(R.id.roleSpinner)
+
+            // Create an ArrayAdapter using the string array and a default spinner layout
+            val adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.roles_array, // Your string array resource
+                android.R.layout.simple_spinner_item
+            )
+
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+            // Apply the adapter to the spinner
+            roleSpinner.adapter = adapter
         }
     }
 }
